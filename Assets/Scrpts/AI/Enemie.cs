61a945dfa2a080b9ea093f396243f9ea0eb17e88
Nolimet <nolimet.stamdata.nl>
2013-12-16 10:02:30 +0100

    Fixed A star

--- Assets/Scrpts/AI/Enemie.cs
+++ Assets/Scrpts/AI/Enemie.cs
@@ -1,28 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Pathfinding;
 
 public class Enemie : MonoBehaviour {
-	private GameObject currentWayPoint;
+
+	public Transform target;
+	public float Speed;
+	public float margenPath;
+	Seeker seeker;
+	Path path;
+	CharacterController CharControler;
+	int currentWaypoint;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		seeker = GetComponent<Seeker>();
+		seeker.StartPath(transform.position,target.position,OnPathComplete);
+		CharControler = GetComponent<CharacterController>();
 	}
-	
+
+	public void OnPathComplete (Path p){
+		if(!p.error){
+			path = p;
+			currentWaypoint=0;
+		}else{
+			Debug.LogError(p.error);
+		}
+	}
 	// Update is called once per frame
-	void Update () {
-		if(currentWayPoint!=null){
-		WayPoint w = currentWayPoint.GetComponent<WayPoint>();
-			transform.rotation = w.direction;
-			transform.localPosition+=new Vector3(1,0);
+	void FixedUpdate () {
+		if(path == null){
+			return;
+		}
+
+		if(currentWaypoint>= path.vectorPath.Count);
+		{
+			return;
+		}
+
+		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized * Speed;
+		CharControler.SimpleMove(dir);
+		if(Vector3.Distance(transform.position,path.vectorPath[currentWaypoint])<margenPath){
+		currentWaypoint++;
 		}
 	}
 
 	void OnTriggerEnter(Collider other){
-		if(other.gameObject.tag=="Waypoint"){
-			if(currentWayPoint!=other.gameObject)
-			{
-				currentWayPoint = other.gameObject;
-			}
-		}
+
 	}
 }
