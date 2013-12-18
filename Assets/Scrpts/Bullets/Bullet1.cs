a874ce887d9b03c4345d08928bcb34e24308ae20
Pepi Dimi <pepi55@live.com>
2013-12-18 09:58:33 +0100

    settings for tower & enemy

--- Assets/Scrpts/Bullets/Bullet1.cs
+++ Assets/Scrpts/Bullets/Bullet1.cs
@@ -5,6 +5,12 @@ public class Bullet1 : MonoBehaviour {
 	GameObject enemy;
 	float bulletSpeed = 200;
 
+	private float xDiff;
+	private float yDiff;
+	
+	private float radians;
+	private float degrees;
+
 	// Use this for initialization
 	void Start () {
 		enemy = GameObject.FindWithTag(GlobalStatics.enemyTag);
@@ -12,10 +18,20 @@ public class Bullet1 : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.LookAt(enemy.transform.position);
+		xDiff = transform.position.x - enemy.transform.position.x;
+		yDiff = transform.position.y - enemy.transform.position.y;
+		
+		radians = Mathf.Atan2(yDiff, xDiff);
+		degrees = (radians * 180) / Mathf.PI;
+
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, degrees);
+
+		//transform.LookAt(enemy.transform.position);
 
 		rigidbody.velocity = Vector3.zero;
-		rigidbody.AddRelativeForce(0, 0, bulletSpeed);
+		rigidbody.AddRelativeForce(-bulletSpeed, 0, 0);
+
+		//transform.localScale = -transform;
 	}
 
 	void OnTriggerEnter (Collider col) {
