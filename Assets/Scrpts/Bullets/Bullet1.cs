e8f8eac82f67197d1934b01aa5f1d63cff46ae52
Pepi Dimi <pepi55@live.com>
2013-12-18 10:24:25 +0100

    added bullet lifetime

--- Assets/Scrpts/Bullets/Bullet1.cs
+++ Assets/Scrpts/Bullets/Bullet1.cs
@@ -2,8 +2,15 @@
 using System.Collections;
 
 public class Bullet1 : MonoBehaviour {
-	GameObject enemy;
-	float bulletSpeed = 200;
+	//GameObjects
+	private GameObject enemy;
+
+	//integers
+	private int lifeTime = 5;
+	private int bulletSpeed = 400;
+
+	//floats
+	private float deathTimer;
 
 	private float xDiff;
 	private float yDiff;
@@ -26,12 +33,16 @@ public class Bullet1 : MonoBehaviour {
 
 		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, degrees);
 
-		//transform.LookAt(enemy.transform.position);
-
 		rigidbody.velocity = Vector3.zero;
 		rigidbody.AddRelativeForce(-bulletSpeed, 0, 0);
 
-		//transform.localScale = -transform;
+		transform.localScale = - transform.localScale;
+
+		deathTimer += 10 * Time.deltaTime;
+
+		if (deathTimer >= lifeTime) {
+			Destroy(gameObject);
+		}
 	}
 
 	void OnTriggerEnter (Collider col) {
