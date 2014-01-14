58ca543929958aa22bf50ff14f183d0144e7ef54
Nolimet <nolimet.stamdata.nl>
2014-01-14 14:55:21 +0100

    added support for multipul tower in a button

--- Assets/Scrpts/buttons/ButtonTowers.cs
+++ Assets/Scrpts/buttons/ButtonTowers.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class ButtonTowers: MonoBehaviour {
 
 	public GameObject Trigger;
-	public GameObject Controling;
+	//public GameObject Controling;
+	public GameObject[] Towers;
 	private Vector2 orginalForce;
 
 	private Sprite SpInactive;
 	public Sprite spActive;
 	private SpriteRenderer spriteRenderer;
+
 	void Start(){
+
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 		SpInactive=spriteRenderer.sprite;
 	}
@@ -18,16 +21,22 @@ public class ButtonTowers: MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D col) {
 		if(col.tag == GlobalStatics.playerTag && Trigger == col.gameObject){
 			spriteRenderer.sprite=spActive;
-			TowerControler conChange = Controling.GetComponent<TowerControler>();
-			conChange.Actived=false;
+
+			foreach (GameObject Controling in Towers){
+				TowerControler conChange = Controling.GetComponent<TowerControler>();
+				conChange.Actived=false;
+			}
 		}
 	}
 	
 	void OnTriggerExit2D (Collider2D col) {
 		if(col.tag == GlobalStatics.playerTag && Trigger == col.gameObject){
 			spriteRenderer.sprite=SpInactive;
-			TowerControler conChange = Controling.GetComponent<TowerControler>();
-			conChange.Actived=true;
+
+			foreach (GameObject Controling in Towers){
+				TowerControler conChange = Controling.GetComponent<TowerControler>();
+				conChange.Actived=true;
+			}
 		}
 	}
 }
