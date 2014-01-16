5ae9766c568d11f6726fd2740babaec6adf9f6b8
Nolimet <nolimet.stamdata.nl>
2014-01-16 11:29:45 +0100

    update

---
+++ Assets/Scrpts/spriteAnimation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spriteAnimation : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public float interval;
+	private float time;
+	private SpriteRenderer renderer;
+	private int index;
+
+	// Use this for initialization
+	void Start () {
+		renderer=GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time+=interval*Time.deltaTime;
+
+		if(time>=interval)
+		{
+			index++;
+			time=0;
+		}
+		if(index>=interval*sprites.Length)
+		{
+			index=0;
+		}
+		renderer.sprite=sprites[index];
+	}
+}
