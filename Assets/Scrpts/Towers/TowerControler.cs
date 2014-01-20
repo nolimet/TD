8f56aa614c7e21da87fa977b37c44d35f2342cfb
Nolimet <nolimet.stamdata.nl>
2014-01-20 10:14:10 +0100

    fixed error spam added if to towercontroler.cs

--- Assets/Scrpts/Towers/TowerControler.cs
+++ Assets/Scrpts/Towers/TowerControler.cs
@@ -29,25 +29,27 @@ public class TowerControler : Tower {
 
 	void Update () {
 		if(!paused){
-			//if (enemy.tag!=GlobalStatics.diggingTag) {
-				if(Actived){
-					//transform.rotation = Quaternion.FromToRotation(Vector3.up - transform.position, enemy.transform.position - transform.position);
-					xDiff = enemy.position.x - transform.position.x; 
-					yDiff = enemy.position.y - transform.position.y;
-					
-					radians = Mathf.Atan2(yDiff, xDiff);
-					degrees = ((radians * 180) / Mathf.PI)+offsetRot;
+			if(enemy){
+				//if (enemy.tag!=GlobalStatics.diggingTag) {
+					if(Actived){
+						//transform.rotation = Quaternion.FromToRotation(Vector3.up - transform.position, enemy.transform.position - transform.position);
+						xDiff = enemy.position.x - transform.position.x; 
+						yDiff = enemy.position.y - transform.position.y;
+						
+						radians = Mathf.Atan2(yDiff, xDiff);
+						degrees = ((radians * 180) / Mathf.PI)+offsetRot;
 
-					transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, degrees);
+						transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, degrees);
 
-					time += fireRate * Time.deltaTime;
+						time += fireRate * Time.deltaTime;
 
-					if (time >= 0.5) {
-						Shoot();
+						if (time >= 0.5) {
+							Shoot();
 
-						time = 0;
-					}
-				//}
+							time = 0;
+						}
+					//}
+				}
 			}
 		}
 	}
